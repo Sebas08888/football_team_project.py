545bf49e2f35fbf9dde7527c7a8ead4af2197e25
Sebas08888 <sebastian.real2008@gmail.com>
2026-08-28 11:44:10 -0500

    Delete Stadium.py

--- Stadium.py
+++
@@ -1,9 +0,0 @@
-class Stadium:
-    def __init__(self, name, capacity, location):
-        self.name = name
-        self.capacity = capacity
-        self.location = location
-
-    def __str__(self):
-        return f"Estadio: {self.name} | Capacidad: {self.capacity} | Ciudad: {self.location}"
-
